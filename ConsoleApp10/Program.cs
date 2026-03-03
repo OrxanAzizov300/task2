a3a4646dd5df463e258b7f31ec5a313015c41cf0
OrxanAzizov300 <orxanezizov062@gmail.com>
2026-03-03 16:53:27 +0400

    task 2 done

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 12;
+            int b = 5;
+            Console.WriteLine(a + b);
+            Console.WriteLine(a - b);
+            Console.WriteLine(a * b);
+            Console.WriteLine(a / b);
+            Console.WriteLine(a % b);
+        }
+    }
+}
