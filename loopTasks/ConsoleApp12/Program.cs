7eee39e32524e8b3520aae692bdafef90cb9e921
OrxanAzizov300 <orxanezizov062@gmail.com>
2026-03-04 17:52:57 +0400

    loop tasks done

---
+++ loopTasks/ConsoleApp12/Program.cs
@@ -0,0 +1,152 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //task1
+            /*for (int i=1; i<10;i++)
+            {
+                Console.WriteLine(i);
+            }*/
+
+
+            //task2
+            /*int cem = 0;
+            for (int i = 0; i < 50; i++)
+            {
+                cem += i;
+            }
+            Console.WriteLine(cem);*/
+
+            //task3
+            /*for (int i = 10; i >0; i--)
+            {
+                Console.WriteLine(i);
+            }*/
+
+            //task4
+            /*int cem = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (i%2==0)
+                {
+                    cem += i;
+                }
+            }
+            Console.WriteLine(cem);*/
+
+
+            //task5
+            /*int cem = 0;
+            for (int i = 0; i < 20; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    cem += i;
+                }
+            }
+            Console.WriteLine(cem);*/
+
+
+            //task6
+            /*string name = "Orxan Azizov";
+            int uzunluq = name.Length;
+            Console.WriteLine(uzunluq);*/
+
+
+            //task7
+            /*int cem = 0;
+              int say = 0
+            for (int i = 0; i < 100; i++)
+            {
+                cem += i;
+                say=say+1
+            }
+            int orta = cem / say;
+            Console.WriteLine(orta);*/
+
+
+            //task8
+            /*for (int i = 1; i < 100; i++)
+            {
+                if (i%5==0 && i%7==0)
+                {
+                    Console.WriteLine(i);
+                }
+            }*/
+
+            //task9
+            /*int cutCem = 0;
+            int tekCem = 0;
+            for (int i = 0; i < 10; i++)
+            {
+                if (i%2==0)
+                {
+                    cutCem += i;
+                }
+                else
+                {
+                    tekCem += i;
+                }
+            }
+            Console.WriteLine(cutCem);
+            Console.WriteLine(tekCem);*/
+
+
+
+            //task10
+            /*int studentGrade = int.Parse(Console.ReadLine());
+            if (studentGrade<70)
+            {
+                Console.WriteLine("UGURSUZ!!!");
+            }
+            else if (studentGrade>69 && studentGrade<80)
+            {
+                Console.WriteLine("c");
+            }
+            else if (studentGrade > 79 && studentGrade < 90)
+            {
+                Console.WriteLine("b");
+            }
+            else if (studentGrade > 89 && studentGrade <=100)
+            {
+                Console.WriteLine("A");
+            }*/
+
+
+            //task11
+            //int[] arr = [12, 14, 35, 56, 68, 93];
+            /*int cem = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                cem += arr[i];
+            }
+
+            task12
+            Console.WriteLine(cem);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i%2==1)
+                {
+                    Console.WriteLine(arr[i]);
+                }
+            }*/
+
+            //task13
+            /*int cem = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    cem += arr[i];
+                }
+            }
+            Console.WriteLine(cem);*/
+
+
+
+
+        }
+    }
+}
